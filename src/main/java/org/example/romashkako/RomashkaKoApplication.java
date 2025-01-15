1f62e7029ee18933a6f231422747f8e60731c183
Spakir <danntyl@mail.ru>
2025-01-15 22:24:30 +0300

    main

---
+++ src/main/java/org/example/romashkako/RomashkaKoApplication.java
@@ -0,0 +1,13 @@
+package org.example.romashkako;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomashkaKoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomashkaKoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/romashkako/RomashkaKoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.romashkako;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomashkaKoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ad3ace2732c577e96a7d77e0517e03ec513c06a5
Spakir <danntyl@mail.ru>
2025-01-17 14:16:48 +0300

    t2 добавление миграции

---
+++ src/main/resources/db/changelog/changeset/create-product.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS Product
+(
+    name VARCHAR(255) PRIMARY KEY NOT NULL,
+    description VARCHAR(4096),
+    price INTEGER DEFAULT 0 CHECK ( price >= 0 ),
+    in_stock BOOLEAN DEFAULT FALSE
+)

146a4b6bc0ae37fe71fc764ccc9438b5aca0e838
Spakir <danntyl@mail.ru>
2025-01-18 10:01:45 +0300

    t1 добавление описания API

--- src/main/java/org/example/romashkako/controller/ProductController.java
+++ src/main/java/org/example/romashkako/controller/ProductController.java
@@ -1,5 +1,8 @@
 package org.example.romashkako.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.example.romashkako.dto.ProductDTO;
 import org.example.romashkako.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,17 +12,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/product/")
+@Api(value = "Product API")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
     @PostMapping("/")
-    public void createProduct(@RequestBody ProductDTO productDTO) {
+    @ApiOperation(value = "Создание нового товара")
+    public void createProduct(@ApiParam(value = "данные добавляемого товара") @RequestBody ProductDTO productDTO) {
         productService.createProduct(productDTO);
     }
 
     @GetMapping("/all")
+    @ApiOperation(value = "Получение всех товаров")
     public List<ProductDTO> getAllProducts() {
         List<ProductDTO> products = productService.getAllProducts();
         return products;

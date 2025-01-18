297ccdb16e6a69527f5effc55058db7367ad4a30
Spakir <danntyl@mail.ru>
2025-01-18 08:44:18 +0300

    t1 обработка ConstraintViolationException

--- src/main/java/org/example/romashkako/handler/RestExceptionHandler.java
+++ src/main/java/org/example/romashkako/handler/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.example.romashkako.handler;
 
+import jakarta.validation.ConstraintViolationException;
 import org.example.romashkako.exception.ProductDoesNotDeletedException;
 import org.example.romashkako.exception.ProductNotFoundException;
 import org.example.romashkako.model.ErrorResponse;
@@ -11,6 +12,12 @@ import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class RestExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException exception){
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), exception.getMessage());
+        return new ResponseEntity<>(errorResponse,HttpStatus.CONFLICT);
+    }
 
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleProductNotFoundException(ProductNotFoundException exception) {

536fe1e9a67a281d2dbbdf7d0a1c0a4bffc44aaf
Spakir <danntyl@mail.ru>
2025-01-19 17:33:05 +0300

    t4 метод фильтрации findByNameContainingIgnoreCase

--- src/main/java/org/example/romashkako/repository/ProductRepository.java
+++ src/main/java/org/example/romashkako/repository/ProductRepository.java
@@ -2,6 +2,9 @@ package org.example.romashkako.repository;
 
 import org.example.romashkako.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    List<Product> findByNameContainingIgnoreCase(String name);
 }

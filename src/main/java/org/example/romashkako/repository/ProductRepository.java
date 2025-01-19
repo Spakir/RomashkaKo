baacb6cd66ff9035eef1a4946a7ea24faf951e52
Spakir <danntyl@mail.ru>
2025-01-19 21:46:28 +0300

    t4 добавление метода findByFilters с SQL запросом

--- src/main/java/org/example/romashkako/repository/ProductRepository.java
+++ src/main/java/org/example/romashkako/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package org.example.romashkako.repository;
 
 import org.example.romashkako.model.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,14 +11,15 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query("""
-                   SELECT p from Product p WHERE 
+                   SELECT p from Product p  WHERE 
                    (:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%',:name,'%'))) AND 
                    (:minPrice IS NULL OR (p.price >= :minPrice)) AND 
                    (:maxPrice IS NULL OR (p.price <= :maxPrice)) AND 
-                   (:inStock IS NULL OR (p.inStock = :inStock))           
+                   (:inStock IS NULL OR (p.inStock = :inStock))
             """)
     List<Product> findByFilters(@Param("name") String name,
                                 @Param("minPrice") Integer minPrice,
                                 @Param("maxPrice") Integer maxPrice,
-                                @Param("inStock") Boolean inStock);
+                                @Param("inStock") Boolean inStock,
+                                Pageable pageable);
 }

995edb4c9ce6825849d200f9c1ff6c1b841ae309
Spakir <danntyl@mail.ru>
2025-01-19 20:02:32 +0300

    t4 добавление метода findByFilters с заданным SQL запросом для фильтрации

--- src/main/java/org/example/romashkako/repository/ProductRepository.java
+++ src/main/java/org/example/romashkako/repository/ProductRepository.java
@@ -2,9 +2,22 @@ package org.example.romashkako.repository;
 
 import org.example.romashkako.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product,Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    List<Product> findByNameContainingIgnoreCase(String name);
+    @Query("""
+                   SELECT p from Product p WHERE 
+                   (:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%',:name,'%'))) AND 
+                   (:minPrice IS NULL OR (p.price >= :minPrice)) AND 
+                   (:maxPrice IS NULL OR (p.price <= :maxPrice)) AND 
+                   (:inStock IS NULL OR (p.inStock = :inStock))           
+            """)
+    List<Product> findByFilters(@Param("name") String name,
+                                @Param("minPrice") Integer minPrice,
+                                @Param("maxPrice") Integer maxPrice,
+                                @Param("inStock") Boolean inStock);
 }

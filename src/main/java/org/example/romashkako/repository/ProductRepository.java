5d85a2c13423aca05c6402535499cfd44177e6ea
Spakir <danntyl@mail.ru>
2025-01-20 10:36:55 +0300

    t4 изменение JPQL запроса

--- src/main/java/org/example/romashkako/repository/ProductRepository.java
+++ src/main/java/org/example/romashkako/repository/ProductRepository.java
@@ -5,19 +5,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query("""
                    SELECT p from Product p  WHERE 
-                   (:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%',:name,'%'))) AND 
+                   (COALESCE(:filterName, '') = '' OR LOWER(p.name) LIKE LOWER(CONCAT('%',:filterName,'%'))) AND 
                    (:minPrice IS NULL OR (p.price >= :minPrice)) AND 
                    (:maxPrice IS NULL OR (p.price <= :maxPrice)) AND 
                    (:inStock IS NULL OR (p.inStock = :inStock))
             """)
-    List<Product> findByFilters(@Param("name") String name,
+    List<Product> findByFilters(@Param("filterName") String filterName,
                                 @Param("minPrice") Integer minPrice,
                                 @Param("maxPrice") Integer maxPrice,
                                 @Param("inStock") Boolean inStock,

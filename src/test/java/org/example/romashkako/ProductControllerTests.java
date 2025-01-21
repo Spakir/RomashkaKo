d9821d31f5dfc9acf745631cf826ab34df434ef3
Spakir <danntyl@mail.ru>
2025-01-21 17:34:19 +0300

    t4 добавление тестов testGetProductById_returnJSONProductDTO и testUpdateProduct_returnJSONUpdatedProductDTO

--- src/test/java/org/example/romashkako/ProductControllerTests.java
+++ src/test/java/org/example/romashkako/ProductControllerTests.java
@@ -1,5 +1,6 @@
 package org.example.romashkako;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.romashkako.controller.ProductController;
 import org.example.romashkako.dto.ProductDTO;
@@ -16,7 +17,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -57,7 +58,6 @@ public class ProductControllerTests {
         );
 
         String jsonRequestContent = objectMapper.writeValueAsString(requestProductDTO);
-
         String jsonResponseContent = objectMapper.writeValueAsString(createdProductDTO);
 
         when(productService.createProduct(requestProductDTO)).thenReturn(createdProductDTO);
@@ -71,5 +71,55 @@ public class ProductControllerTests {
         verify(productService,times(1)).createProduct(requestProductDTO);
     }
 
+    @Test
+    public void testGetProductById_returnJSONProductDTO() throws Exception {
+        Long existProductId = 1L;
+        ProductDTO existProductDto = new ProductDTO(
+                existProductId,
+                "Product",
+                "Product description",
+                10,
+                true
+        );
+        String jsonResponse = objectMapper.writeValueAsString(existProductDto);
 
+        when(productService.getProductById(existProductId)).thenReturn(existProductDto);
+
+        mockMvc.perform(get("/api/product/{id}",existProductId))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(jsonResponse));
+
+        verify(productService,times(1)).getProductById(1L);
+    }
+
+    @Test
+    public void testUpdateProduct_returnJSONUpdatedProductDTO() throws Exception {
+        Long existProductId = 1L;
+        ProductDTO productDTOForUpdate = new ProductDTO(
+                "new Product",
+                "new product description",
+                2,
+                true
+        );
+        ProductDTO updatedProductDTO = new ProductDTO(
+                existProductId,
+                productDTOForUpdate.getName(),
+                productDTOForUpdate.getDescription(),
+                productDTOForUpdate.getPrice(),
+                productDTOForUpdate.isInStock()
+        );
+
+        String jsonRequestContent = objectMapper.writeValueAsString(productDTOForUpdate);
+        String jsonResultContent = objectMapper.writeValueAsString(updatedProductDTO);
+
+        when(productService.updateProduct(existProductId,productDTOForUpdate)).thenReturn(updatedProductDTO);
+
+        mockMvc.perform(put("/api/product/{id}",existProductId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonRequestContent))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(jsonResultContent));
+
+        verify(productService,times(1)).updateProduct(existProductId,productDTOForUpdate);
+    }
 }

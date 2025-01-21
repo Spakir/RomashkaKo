445ff0f0662d348e98f369bb092abf5766a0b6b1
Spakir <danntyl@mail.ru>
2025-01-21 14:05:54 +0300

    t4 обработка нового исключения MethodArgumentNotValidException

--- src/main/java/org/example/romashkako/handler/RestExceptionHandler.java
+++ src/main/java/org/example/romashkako/handler/RestExceptionHandler.java
@@ -2,9 +2,11 @@ package org.example.romashkako.handler;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.ValidationException;
 import org.example.romashkako.model.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -20,7 +22,7 @@ public class RestExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException exception) {
-        return createResponseEntity(exception, HttpStatus.CONFLICT);
+        return createResponseEntity(exception, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
@@ -30,11 +32,24 @@ public class RestExceptionHandler {
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception) {
-        return createResponseEntity(exception, HttpStatus.CONFLICT);
+        return createResponseEntity(exception, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        Exception defaultValidationException = null;
+        if(!exception.getBindingResult().getFieldErrors().isEmpty()){
+            String errorMessage = exception.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+            defaultValidationException = new Exception(errorMessage);
+        }else{
+            defaultValidationException = new Exception("Ошибка валидации");
+        }
+
+        return createResponseEntity(defaultValidationException,HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(IllegalAccessError.class)
     public ResponseEntity<ErrorResponse> handleIllegalAccessError(IllegalAccessError error) {
-        return createResponseEntity(error, HttpStatus.CONFLICT);
+        return createResponseEntity(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }

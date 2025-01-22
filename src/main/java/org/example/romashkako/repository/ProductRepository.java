f9232fb661b2db43a7876faada2781445b522181
Spakir <danntyl@mail.ru>
2025-01-22 11:24:54 +0300

    t4 изменение Запроса к БД с JPQL на SQL

--- src/main/java/org/example/romashkako/repository/ProductRepository.java
+++ src/main/java/org/example/romashkako/repository/ProductRepository.java
@@ -9,16 +9,27 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query("""
-                   SELECT p from Product p  WHERE 
-                   (COALESCE(:filterName, '') = '' OR LOWER(p.name) LIKE LOWER(CONCAT('%',:filterName,'%'))) AND 
-                   (:minPrice IS NULL OR (p.price >= :minPrice)) AND 
-                   (:maxPrice IS NULL OR (p.price <= :maxPrice)) AND 
-                   (:inStock IS NULL OR (p.inStock = :inStock))
-            """)
+    @Query(value = """
+            SELECT * FROM products p 
+                     
+            WHERE (COALESCE(:filterName, '') = '' OR LOWER(p.name) LIKE LOWER(CONCAT('%', :filterName, '%'))) AND 
+                  (:minPrice IS NULL OR (p.price >= :minPrice)) AND 
+                  (:maxPrice IS NULL OR (p.price <= :maxPrice)) AND 
+                  (:inStock IS NULL OR (p.in_stock = :inStock))
+            
+            ORDER BY CASE WHEN :sortType = 'name' AND :sortDirection = 'ASC' THEN p.name END ASC,
+                     CASE WHEN :sortType = 'name' AND :sortDirection = 'DESC' THEN p.name END DESC,
+                     CASE WHEN :sortType = 'price' AND :sortDirection = 'ASC' THEN p.price END ASC,
+                     CASE WHEN :sortType = 'price' AND :sortDirection = 'DESC' THEN p.price END DESC
+            
+            LIMIT :limit OFFSET :offset
+            """,nativeQuery = true)
     List<Product> findByFilters(@Param("filterName") String filterName,
                                 @Param("minPrice") Integer minPrice,
                                 @Param("maxPrice") Integer maxPrice,
                                 @Param("inStock") Boolean inStock,
-                                Pageable pageable);
+                                @Param("sortType") String sortType,
+                                @Param("sortDirection") String sortDirection,
+                                @Param("limit") int limit,
+                                @Param("offset") int offset);
 }

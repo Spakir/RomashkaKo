2726f5adbdcab1a5306f0f9a95c617482c95b9fc
Spakir <danntyl@mail.ru>
2025-02-05 17:01:54 +0300

    t5 добавление метода updateProductFromDTO

--- src/main/java/org/example/romashkako/mapper/ProductMapper.java
+++ src/main/java/org/example/romashkako/mapper/ProductMapper.java
@@ -3,10 +3,15 @@ package org.example.romashkako.mapper;
 import org.example.romashkako.dto.ProductDTO;
 import org.example.romashkako.model.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     ProductDTO toProductDTO(Product product);
 
     Product toProduct(ProductDTO productDTO);
+
+    @Mapping(target = "id", ignore = true)
+    void updateProductFromDTO(ProductDTO productDTO, @MappingTarget Product product);
 }

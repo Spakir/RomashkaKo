febed70983000936fb9dcc3f7f8865f20793771d
Spakir <danntyl@mail.ru>
2025-02-06 17:22:38 +0300

    t5 Реализация ProductSupplyService

---
+++ src/main/java/org/example/romashkako/service/ProductSupplyServiceImpl.java
@@ -0,0 +1,84 @@
+package org.example.romashkako.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.example.romashkako.dto.ProductSupplyDTO;
+import org.example.romashkako.mapper.ProductMapper;
+import org.example.romashkako.mapper.ProductSupplyMapper;
+import org.example.romashkako.model.ProductSupply;
+import org.example.romashkako.repository.ProductSupplyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductSupplyServiceImpl implements ProductSupplyService {
+
+    private final ProductSupplyRepository productSupplyRepository;
+
+    private final ProductSupplyMapper productSupplyMapper;
+
+    private final ProductService productService;
+
+    private final ProductMapper productMapper;
+
+    @Autowired
+    public ProductSupplyServiceImpl(ProductSupplyRepository productSupplyRepository,
+                                    ProductSupplyMapper productSupplyMapper,
+                                    ProductService productService,
+                                    ProductMapper productMapper) {
+        this.productSupplyMapper = productSupplyMapper;
+        this.productSupplyRepository = productSupplyRepository;
+        this.productService = productService;
+        this.productMapper = productMapper;
+    }
+
+    @Override
+    public ProductSupplyDTO createProductSupply(ProductSupplyDTO productSupplyDTO) {
+        ProductSupply productSupply = mapToProductSupply(productSupplyDTO);
+        ProductSupply createdProductSupply = productSupplyRepository.save(productSupply);
+        return mapToProductSupplyDTO(createdProductSupply);
+    }
+
+    @Override
+    public ProductSupplyDTO getProductSupplyById(Long id) {
+        ProductSupply productSupply = getExistProductSupplyOrThrow(id);
+        return mapToProductSupplyDTO(productSupply);
+    }
+
+    @Override
+    public List<ProductSupplyDTO> getAllProductSupplies() {
+        return productSupplyRepository.findAll()
+                .stream()
+                .map(this::mapToProductSupplyDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public ProductSupplyDTO updateProductSupply(Long id, ProductSupplyDTO productSupplyDTO) {
+        ProductSupply existProductSupply = getExistProductSupplyOrThrow(id);
+        productSupplyMapper.updateProductSuppleFromDTO(productSupplyDTO, existProductSupply);
+        ProductSupply updatedProductSupply = productSupplyRepository.save(existProductSupply);
+        return mapToProductSupplyDTO(updatedProductSupply);
+    }
+
+    @Override
+    public void deleteProductSupplyById(Long id) {
+        getExistProductSupplyOrThrow(id);
+        productSupplyRepository.deleteById(id);
+    }
+
+    private ProductSupply getExistProductSupplyOrThrow(Long id) {
+        return productSupplyRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Поставка товара с данным ID не была найдена"));
+    }
+
+    private ProductSupply mapToProductSupply(ProductSupplyDTO productSupplyDTO) {
+        return productSupplyMapper.toProductSupply(productSupplyDTO, productService, productMapper);
+    }
+
+    private ProductSupplyDTO mapToProductSupplyDTO(ProductSupply productSupply) {
+        return productSupplyMapper.toProductSupplyDTO(productSupply);
+    }
+}

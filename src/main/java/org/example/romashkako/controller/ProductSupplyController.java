943665c7bd5e6383965b1afd2662ba790cfff713
Spakir <danntyl@mail.ru>
2025-02-06 17:22:53 +0300

    t5 Контроллер для взаимодействия с поставками товара

---
+++ src/main/java/org/example/romashkako/controller/ProductSupplyController.java
@@ -0,0 +1,56 @@
+package org.example.romashkako.controller;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.example.romashkako.dto.ProductSupplyDTO;
+import org.example.romashkako.service.ProductSupplyService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/supply/")
+public class ProductSupplyController {
+
+    private final ProductSupplyService productSupplyService;
+
+    public ProductSupplyController(ProductSupplyService productSupplyService) {
+        this.productSupplyService = productSupplyService;
+    }
+
+    @PostMapping("/")
+    @ApiOperation("Создание новой поставки товара")
+    public ProductSupplyDTO createProductSupply(@ApiParam(value = "Данные добавляемой поставки товара")
+                                                @RequestBody ProductSupplyDTO productSupplyDTO) {
+        return productSupplyService.createProductSupply(productSupplyDTO);
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation("Получение поставки товара по ID")
+    public ProductSupplyDTO getProductSupplyOById(@ApiParam(value = "id поставки товара для получения")
+                                                  @PathVariable(name = "id") Long id) {
+        return productSupplyService.getProductSupplyById(id);
+    }
+
+    @GetMapping("/all")
+    @ApiOperation("Получение всех поставок товара")
+    public List<ProductSupplyDTO> getAllProductsSupplies() {
+        return productSupplyService.getAllProductSupplies();
+    }
+
+    @PutMapping("/{id}")
+    @ApiOperation("Обновление поставки товара по ID")
+    public ProductSupplyDTO updateProductSupply(@ApiParam(value = "ID поставки товара,которую надо обновить")
+                                                @PathVariable(name = "id") Long id,
+                                                @ApiParam(value = "DTO поставки товара,которую надо обновить")
+                                                @RequestBody ProductSupplyDTO productSupplyDTO) {
+        return productSupplyService.updateProductSupply(id, productSupplyDTO);
+    }
+
+    @DeleteMapping("{id}")
+    @ApiOperation("Удаление поставки товара по ID")
+    public void deleteProductSupply(@ApiParam(value = "ID поставки товара,которую надо удалить")
+                                    @PathVariable(name = "id") Long id) {
+        productSupplyService.deleteProductSupplyById(id);
+    }
+}

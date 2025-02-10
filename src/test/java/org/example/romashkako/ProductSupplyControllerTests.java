20b80ad6a2ab64c7c8eb982710adca035ef27ea7
Spakir <danntyl@mail.ru>
2025-02-10 20:41:17 +0300

    t5 удаление неиспользуемой зависимости

--- src/test/java/org/example/romashkako/ProductSupplyControllerTests.java
+++ src/test/java/org/example/romashkako/ProductSupplyControllerTests.java
@@ -3,8 +3,9 @@ package org.example.romashkako;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.romashkako.controller.ProductSupplyController;
+import org.example.romashkako.dto.ProductDTO;
 import org.example.romashkako.dto.ProductSupplyDTO;
-import org.example.romashkako.model.Product;
+import org.example.romashkako.service.ProductSupplyService;
 import org.example.romashkako.service.ProductSupplyServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,9 +17,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
+import java.util.List;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -54,11 +56,73 @@ public class ProductSupplyControllerTests {
         when(productSupplyService.createProductSupply(productSupplyDTO)).thenReturn(createdProductSupply);
 
         mockMvc.perform(post("/api/supply/")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json(jsonResponse));
 
-        verify(productSupplyService,times(1)).createProductSupply(productSupplyDTO);
+        verify(productSupplyService, times(1)).createProductSupply(productSupplyDTO);
+    }
+
+    @Test
+    public void test_getProductSupplyById() throws Exception {
+        Long id = 1L;
+        String documentName = "document";
+        int quantity = 100;
+
+        ProductSupplyDTO productSupplyDTO = new ProductSupplyDTO(id, documentName, id, quantity);
+        String jsonResponse = objectMapper.writeValueAsString(productSupplyDTO);
+
+        when(productSupplyService.getProductSupplyById(id)).thenReturn(productSupplyDTO);
+
+        mockMvc.perform(get("/api/supply/{id}", id))
+                .andExpect(MockMvcResultMatchers.content().json(jsonResponse));
+
+        verify(productSupplyService, times(1)).getProductSupplyById(id);
+    }
+
+    @Test
+    public void test_getAllProductsSupplies() throws Exception {
+        Long id = 1L;
+        String documentName = "document";
+        int quantity = 100;
+
+        ProductSupplyDTO productSupplyDTO = new ProductSupplyDTO(id, documentName, id, quantity);
+        List<ProductSupplyDTO> list = List.of(productSupplyDTO);
+        String jsonResponse = objectMapper.writeValueAsString(list);
+
+        when(productSupplyService.getAllProductSupplies()).thenReturn(list);
+
+        mockMvc.perform(get("/api/supply/all"))
+                .andExpect(MockMvcResultMatchers.content().json(jsonResponse));
+
+        verify(productSupplyService,times(1)).getAllProductSupplies();
+    }
+
+    @Test
+    public void test_updateProductSupply() throws Exception {
+        Long id = 1L;
+        String documentName = "document";
+        int quantity = 100;
+
+        ProductSupplyDTO updateProductSupplyDTO = new ProductSupplyDTO(null,documentName,id,quantity);
+        ProductSupplyDTO updatedProductSupplyDTOWithId = new ProductSupplyDTO(id,documentName,id,quantity);
+        String jsonRequest = objectMapper.writeValueAsString(updateProductSupplyDTO);
+        String jsonResponse = objectMapper.writeValueAsString(updatedProductSupplyDTOWithId);
+
+        when(productSupplyService.updateProductSupply(id,updateProductSupplyDTO))
+                .thenReturn(updatedProductSupplyDTOWithId);
+
+        mockMvc.perform(put("/api/supply/{id}",id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonRequest))
+                .andExpect(MockMvcResultMatchers.content().json(jsonResponse));
+
+        verify(productSupplyService,times(1)).updateProductSupply(id,updateProductSupplyDTO);
+    }
+
+    @Test
+    public void test_deleteProductSupply(){
+
     }
 }

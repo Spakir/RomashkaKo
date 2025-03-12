52511e13ee9074e9efafd1ec1597b88d8bb086f0
Spakir <danntyl@mail.ru>
2025-03-12 20:51:17 +0300

    t6 решение проблемы N + 1 для ProductSupply

--- src/main/java/org/example/romashkako/repository/ProductSupplyRepository.java
+++ src/main/java/org/example/romashkako/repository/ProductSupplyRepository.java
@@ -2,8 +2,14 @@ package org.example.romashkako.repository;
 
 import org.example.romashkako.model.ProductSupply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ProductSupplyRepository extends JpaRepository<ProductSupply,Long> {
+public interface ProductSupplyRepository extends JpaRepository<ProductSupply, Long> {
+
+    @Query("SELECT ps FROM ProductSupply ps JOIN FETCH ps.product")
+    List<ProductSupply> findAll();
 }

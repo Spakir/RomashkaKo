ef83f4cee1ba555c8d6530e0e275b26b3e828b83
Spakir <danntyl@mail.ru>
2025-03-12 20:52:21 +0300

    t6 конфигурация Time to leave для каждой сущности, указанной в конфиге cache-config.yaml

--- src/main/java/org/example/romashkako/configuration/RedisConfiguration.java
+++ src/main/java/org/example/romashkako/configuration/RedisConfiguration.java
@@ -1,5 +1,7 @@
 package org.example.romashkako.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -11,23 +13,62 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 @EnableCaching
+@ConfigurationProperties(prefix = "cache")
 public class RedisConfiguration {
+
+    private Map<String, Long> ttl;
+
+    public Map<String, Long> getTtl() {
+        return ttl;
+    }
+
+    public void setTtl(Map<String, Long> ttl) {
+        System.out.println("Loaded TTL values: " + ttl);
+        this.ttl = ttl;
+    }
 
     @Bean
     CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        RedisCacheConfiguration configuration = RedisCacheConfiguration
-                .defaultCacheConfig()
+        RedisCacheConfiguration defaultCache = baseCacheConfiguration();
+
+        Map<String, RedisCacheConfiguration> map = new HashMap<>();
+        for(Map.Entry<String, Long> entry : ttl.entrySet()){
+            String cacheName = entry.getKey();
+            Long ttlSeconds = entry.getValue();
+            map.put(cacheName,
+                    createCacheConfigWithTimeToLeave(Duration.ofSeconds(ttlSeconds)));
+        }
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(defaultCache)
+                .withInitialCacheConfigurations(map)
+                .build();
+    }
+
+    private RedisCacheConfiguration createCacheConfigWithTimeToLeave(Duration ttl) {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(ttl)
                 .serializeKeysWith(RedisSerializationContext
                         .SerializationPair
                         .fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext
                         .SerializationPair
                         .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
 
-        return RedisCacheManager.builder(redisConnectionFactory)
-                .cacheDefaults(configuration)
-                .build();
+    private RedisCacheConfiguration baseCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
     }
 }
